25939ec1d08dae3001c8bec08e3e39e4f3117648
Jason Whyttes <jwhyttes@gmail.com>
2017-07-14 20:23:00 +1000

    rough draft calcualte seconds in life (non specfic time set)

--- 03 - calculate age in seconds.py
+++ 03 - calculate age in seconds.py
@@ -1,73 +1,85 @@
-##############################################
-#             by Jason Whyttes               #
-#  this script will calculate the users age  #
-#    in seconds and factor in leap years     #
-#                                            #
-#   -----------How old are you?-----------   #
-#                                            #
-#   Enter day you were born     -            #
-#   Enter month you were born   -            #
-#   Enter year you were born    -            #
-#   Please use numbers          -            #
-#                                            #
-#   What time were you born     -            #
-#                                            #
-#   You have been alive for     - ""seconds  #
-#                                            #
-#   Check for leap years                     #
-#   Get system date and time                 #
-#   Get total days since birtday             #
-#   Check time format 24 hr or not           #
-#   If time is over 12:59 (eg 13:00)         #
-#   Use 24 hour time and dont ask for AM/PM  #
-#   Else as for AM or PM                     #
-##############################################
-from time import gmtime, strftime
-from calendar import isleap
-#use above function to get leap year otherwise
-#use year % 4 == 0 and ( year % 100 != 0 or year % 400 == 0 )
-#   ^first term         ^second and third term first
-#works on premis of
-#1 - year evenly div by 4           (yes = 2, no = 5)
-#2 - year evenly div by 100         (yes = 3, no = 4)
-#3 - year evenly div by 400         (yes = 4, no = 5)
-#4 - year has 366 days
-#5 - year has 365 days
-#we are going to use 24 hour time so we do not need to think about AM/PM conversion later on except on if user enters with AM/PM
-curTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+from calendar import isleap, monthrange
+from datetime import *
 
-#gets user info for birthday, checks input for type and length, max 31, we will check values later on
-def userInfo():
-    lstOfMonths['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER','1','2','3','4','5','6','7','8','9','10','11','12','01','02','03','04','05','06','07','08','09','JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
-    print('-----------How old are you?-----------\n\n')
-    userDay = raw_input("Enter day you were born     - ")
+def getDay(maxDays):
+    userInput = raw_input("Ender the day you were born   - ")
     while True:
         try:
-            toInt = int(userDay)
-            if toInt <= 31 and toInt >= 0:
-                userDay = toInt
+            intTest = int(userInput)
+            if intTest <=maxDays and intTest >= 0:
+                userDay = intTest
                 break
             else:
-                userDay = raw_input("Please use a valid date     - ")
+                userInput = raw_input("Please use a valid date       - ")
         except ValueError:
-            userDay = raw_input("Please use numbers          - ")
-    userMonth = raw_input("Enter month you were born   - ")
-    #while True:
-    userYear = raw_input("Enter year you were born    - ")
-    print("\n")
-    userTime = raw_input("What time were you born     - ")
-    print("\n")
-    return [userDay,userMonth,userYear,userTime]
+            userInput = raw_input("Please use numbers            - ")
+    return userDay
 
+def getMonth(year):
+    full = ['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER']
+    short = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+    integersv1 = ['1','2','3','4','5','6','7','8','9','10','11','12']
+    integersv2 = ['01','02','03','04','05','06','07','08','09']
 
+    userInput = raw_input("Enter the month you were born - ").upper()
+    while True:
+        if userInput in full:
+            break
+        elif userInput in short or userInput in integersv1 or userInput in integersv2:
+            target = [i for i in [full,short,integersv1,integersv2] if userInput in i]
+            pos = target[0].index(userInput)
+            userInput = full[pos]
+            break
+        else:
+            userInput = raw_input("Please enter a valid month    - ").upper()
+    daysInMonth = monthrange( year, full.index(userInput) + 1 )[1]
+    dayNum = full.index(userInput)
+    return userInput, daysInMonth, dayNum
 
-userInput = userInfo()
-#checks if the input exists in 'history' if not it will re-run the userInfo funciton
-def checkDayExists(day,month,year):
-    print(userInput)
+def getYear():
+    leap = 0
+    userInput = raw_input("Enter the year you were born  - ")
+    while True:
+        try:
+            intTest = int(userInput)
+            userInput = intTest
+            break              
+        except ValueError:
+            raw_input("Please use numbers            - ")
+    return userInput
 
-v = checkDayExists(userInput[0],userInput[1],userInput[2])
+def leapyear(year):
+    leap = isleap(year)
+    return leap
 
+def countSec(year,month,day):
+    birthday = date(year,month,day)
+    today = date.today()
+    deltaTime = today - birthday
+    days = deltaTime.days
+    seconds = days * 24 * 60 * 60
+    return seconds
 
+def intWithCommas(x):
+    if type(x) not in [type(0), type(0L)]:
+        raise TypeError("Parameter must be an integer.")
+    if x < 0:
+        return '-' + intWithCommas(-x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (x, result)
 
-
+year = getYear()
+leap = leapyear(year)
+month = getMonth(2016)
+day = getDay(month[1])
+seconds = countSec(int(year),int(month[2]),int(day))
+timeInSeconds = intWithCommas(seconds)
+print(day)
+print(month[0].capitalize())
+print(year)
+print('leap years - ' + str(leap))
+print('not calculating leap years currently, results will be off by a little bit.')
+print('You have been alive for ' + timeInSeconds + ' seconds.')
