500a33deff35ffa9771d078fb75db437c5a06f42
Jason Whyttes <jwhyttes@gmail.com>
2017-07-21 00:02:16 +1000

    Changed the way it works

--- 06 - Rock Paper Scissors Lizard Spock.py
+++ 06 - Rock Paper Scissors Lizard Spock.py
@@ -1,63 +1,62 @@
-# The Rock Paper Scissors Lizard Spock game         #
-# has two algorithms to determin a win              #
-# assign the user choice to a 1-5 number scheme     #
-# respective of the games titles name               #
-# if the choice is 2 or 3 minus 1 or add 2          #
-# if the computers choice is the outcome the        #
-# user won, if the user chose 1 or 5 we add 2       #
-# or 3 and check the answer for the computers       #
-# choice. to get around the 5 + 2/3 being out of    #
-# range, we also assign 6 and 7 to the computers    #
-# choice respective of 1 and 2, that way if the     #
-# user chooses 5 it will add 1 or 2 and come out    #
-# 6 or 7, the exception here is 4, where we check   #
-# + 1 and - 1                                       #
-#####################################################
 
-# we will start by getting the computers input from
-# a random int 1-5, then check if it is 1 or 2 and
-# if so add 6 or 7 to its list respectively
-from random import randint
-cg = [randint(1,5)]
+import random
 
-if cg == [1]:
-    cg.append(7)
-if cg == [2]:
-    cg.append(8)
+def objStr(num):
+    if num == 0:
+        result = "rock"
+    elif num == 1:
+        result = "Spock"
+    elif num == 2:
+        result = "paper"
+    elif num == 3:
+        result = "lizard"
+    elif num == 4:
+        result = "scissors"
+    return result
 
-# now we need to get the users input and apply some
-# rules to it (number is between 1 and 5 and is a int)
-while True:
-    try:
-        ug = int(input('1     -     Rock\n2     -     Spock\n3     -     Lizard\n4     -     Scissors\n5     -     Paper\n> '))
-        if ug > 5 or ug < 1:
-            ug = int(input('1     -     Rock\n2     -     Spock\n3     -     Lizard\n4     -     Scissors\n5     -     Paper\n> '))
-        break
-    except ValueError:
-        ug = int(input('1     -     Rock\n2     -     Spock\n3     -     Lizard\n4     -     Scissors\n5     -     Paper\n> '))
-        ug = int(input('Use Numbers.\n> '))
-# now we check if the output of the user with our rules
-grpA = [1,5]
-grpB = [2,3]
-grpC = [4]
-if ug == cg:
-    print('its a tie')
-if ug in grpA:
-    for i in cg:
-        if i == ug -1 or i == ug + 2:
-            print('You won')
+
+def objInt(name):
+
+
+    if name == "rock":
+        result = 0
+    elif name == "Spock":
+        result = 1
+    elif name == "paper":
+        result = 2
+    elif name == "lizard":
+        result = 3
+    elif name == "scissors":
+        result = 4
+    return result
+
+
+def game():
+    accInp = ['rock','spock','paper','lizard','scissors']
+    ug = input('Choose one...\n\nRock\nSpock\nPaper\nLizard\nScissors\n> ')
+    while True:
+        if ug in accInp or ug.capitalize() in accInp:
+            ug = ug.lower()
+            break
         else:
-            print('You lost')
-if ug in grpB:
-    for i in cg:
-        if i == ug + 2 or i == ug + 3:
-            print('You won')
-        else:
-            print('You lost')
-if ug in grpC:
-    for i in cg:
-        if i == ug -1 or i == ug + 1:
-            print('You won')
-        else:
-            print('You lost')
-print(cg)
+            ug = input('Incorrect input...')
+        
+    obj = objInt(ug)
+    cg = random.randint(1, 5)
+
+    print ("Player chooses", objStr(obj))
+    print ("Computer chooses", objStr(cg))
+
+    if (cg + 1) % 5 == ug or (cg + 2) % 5 == ug:
+        print ("Player wins!")
+    elif cg == ug:
+        print ("Player and computer tie!")
+    else:
+        print ("Computer wins!")
+    print ("")
+game()
+#game("rock")
+#game("Spock")
+#game("paper")
+#game("lizard")
+#game("scissors")
