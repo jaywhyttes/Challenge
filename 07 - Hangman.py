293b5253b6669f8238dc448c274ffb959535a155
Jason Whyttes <jwhyttes@gmail.com>
2017-07-30 16:07:15 +1000

    updated Hangman game and the output of the getWordsOxford.py in a .txt file for use with the Hangman game, game has three working functions for difficulty and successfully pulls words from the database in correct format.

--- 07 - Hangman.py
+++ 07 - Hangman.py
@@ -1,8 +1,7 @@
-"""
-Version: 1 | Date: 30/07/2017 | Developed: Python 2.7.13
-Handman game
-Jason Whyttes
-"""
+# Version: 2 | Date: 30/07/2017 | Python 2.7.13
+# Handman game
+# By Jason Whyttes
+
 from re import findall
 from urllib import urlopen as uReq
 from bs4 import BeautifulSoup as soup
@@ -10,36 +9,19 @@ from random import randint
 #some initial tests for layout and code functions
 
 #difficulty determins word length
-#easy <= 5 letters
-#medium >= 5 <= 10 letters
-#hard >= 10 letters
-#expert 2 words >= 5 letters each
-#determined with numbers 0-3, 0 being easy and 3 expert
-"""
-def getWords():
-    url = 'https://www.randomlists.com/random-words'
-    uClient = uReq(url)
-    page_html = uClient.read()
-    uClient.close()
-    page_soup = soup(page_html, "html.parser")
-    containerName = page_soup.findAll("span",{"class":"crux"})
-    word = findall(r'">(.*?)<',str(containerName))  
-    return word
-"""
-def genList():
-    bigList = ['copper', 'explain', 'ill-fated', 'truck', 'neat', 'unite', 'branch', 'educated', 'tenuous', 'hum', 'decisive', 'notice', 'copper', 'explain', 'ill-fated', 'truck', 'neat', 'unite', 'branch', 'educated', 'tenuous', 'hum', 'decisive', 'notice', 'copper', 'explain', 'ill-fated', 'truck', 'neat', 'unite', 'branch', 'educated', 'tenuous', 'hum', 'decisive', 'notice', 'copper', 'explain', 'ill-fated', 'truck', 'neat', 'unite', 'branch', 'educated', 'tenuous', 'hum', 'decisive', 'notice', 'copper', 'explain', 'ill-fated', 'truck', 'neat', 'unite', 'branch', 'educated', 'tenuous', 'hum', 'decisive', 'notice']
-    # The scrapping works so we will give the server a break for now and
-    # use this list I created from the previous scrape
-    """
-    while True:
-        if len(bigList) < 50:
-            for words in getWords():
-                bigList.append(words)
-        else:
-            break
-    """
-    return bigList
-    
+#easy <= 5 characters
+#medium >= 5 <= 10 characters
+#hard >= 10 characters
+
+def readData():
+    data = open('hangmanWordsData.txt', 'r')
+    lines = data.readlines()
+    words = []
+    for item in lines:
+        words.append(item[:-1])
+    data.close()
+    return words
+
 def genEasy(words):
     easyWords = []
     for word in words:
@@ -55,6 +37,7 @@ def genMedium(words):
             medWords.append(word)
     word = medWords[randint(0, len(medWords))]
     return word
+
 def genHard(words):
     hardWords = []
     for words in words:
@@ -63,5 +46,7 @@ def genHard(words):
     word = hardWords[randint(0, len(hardWords))]
     return word
 
-print(genHard(genList()))
-#getWords()
+data = readData()
+#(genEasy(data))
+#(genMedium(data))
+#(genHard(data))
